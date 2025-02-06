3d6a527e6a13023154b26b758c7690b842319276
Bhishmaaa <bhishm2613@gmail.com>
2025-02-06 10:19:33 +0000

    conversion of datatypes

---
+++ 01_basics/conversion.js
@@ -0,0 +1,8 @@
+let score = "33"
+//we can set any data type in simple let call
+console.log(typeof score)
+let intNumber = Number(score)// converting into number
+console.log(intNumber);
+console.log(typeof intNumber);
+ // what if the string is some thing like this 33abs? then also it will be convert it into the number with NAN value
+ // if null-> so 0 if true then 1 false then 0 if  string with character then NAN.
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,5 @@
 console.log("run hoja bro")
+let bhishma = 10
+let shreya = bhishma
+
+console.log(shreya);
